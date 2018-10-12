9e196f0badb233e4bbc12546b6d00ce452057356
rishavm47 <43958150+rishavm47@users.noreply.github.com>
2018-10-12 22:59:10 +0530

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	cout<<"Hello Github"<<endl;
-	cout<<"My name is Kunal"<<endl;
+	cout<<"My name is Mclovin Kunal"<<endl;
 	cout<<"Even my name is Kunal, LOL!"<<endl;
 	return 0;
 }
